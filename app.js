fa92a0a650b0991c19b4aedc685eafb67c08dae9
Cade Nichols <c@codinghouse.co>
2016-08-06 11:54:27 -0700

    basic app.js stuff

---
+++ app.js
@@ -0,0 +1,25 @@
+// CONSTANTS
+const PORT = process.env.PORT || 8000;
+
+// REQUIRES
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// APP DECLARATION
+const app = express();
+
+// GENERAL MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// ROUTES
+
+
+
+// SERVER LISTEN
+app.listen(PORT, err => {
+  console.log(err || `Server listening on port ${PORT}`);
+});
